72abe229912306970e682953ab88c88ac5e95334
Vikas Kumar Gautam <vikacse@gmail.com>
2024-07-22 01:52:30 +0530

    Singly Linked List

---
+++ linked-list/helper/helper.go
@@ -0,0 +1,54 @@
+package helper
+
+import (
+	"fmt"
+)
+
+type LinkedList struct {
+	Value int
+	Next  *LinkedList
+}
+type Head struct {
+	Head *LinkedList
+}
+
+func (h *Head) InsertValueInList() {
+	slice := []int{1, 4, 8, 2, 5, 9, 3, 6}
+	currentNode := h.Head
+	for i := 0; i < len(slice); i++ {
+		node := &LinkedList{}
+		node.Value = slice[i]
+		node.Next = nil
+		if currentNode == nil {
+			h.Head = node
+			currentNode = node
+		} else {
+			currentNode.Next = node
+			currentNode = node
+		}
+	}
+	fmt.Println("Inserted Node and return head ", h.Head)
+}
+
+func (h *Head) GetAllNode() []int {
+	currentNode := h.Head
+	var slices []int
+	for currentNode != nil {
+		slices = append(slices, currentNode.Value)
+		currentNode = currentNode.Next
+	}
+	fmt.Println("Get all nodes ", slices)
+	return slices
+}
+
+func (h *Head) DeleteFirstNode() {
+	currentNode := h.Head
+	h.Head = currentNode.Next
+	currentNode = h.Head
+	var slice []int
+	for currentNode != nil {
+		slice = append(slice, currentNode.Value)
+		currentNode = currentNode.Next
+	}
+	fmt.Println("Delete First Node from list ", slice)
+}
---
+++ linked-list/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"main.go/helper"
+)
+
+func main() {
+	fmt.Println("hello linked list")
+	head := helper.Head{}
+	head.InsertValueInList()
+	head.GetAllNode()
+	head.DeleteFirstNode()
+}
